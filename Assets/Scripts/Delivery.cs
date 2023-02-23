ff3a974ea7b5982db2f463bc3652e5e06bf31bca
Sayan Bose <sayan.bose.lmg.2000@gmail.com>
2023-02-23 12:06:52 +0530

    Added a functionality by which the car changes color when it picks up a vaccine, and resets the color once it has delivered to the patient.

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -5,7 +5,18 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
-    private bool _hasVaccine; 
+    private bool _hasVaccine;
+    [SerializeField] private Color32 hasPackageColor = new Color32(243, 104, 26, 255);
+    [SerializeField] private Color32 noPackageColor = new Color32(255, 255, 255, 255);
+    [SerializeField] private float destructionDelay = 0.05f;
+
+    private SpriteRenderer _spriteRenderer;
+
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     private void OnCollisionEnter2D(Collision2D col)
     {
         // Debug.Log("Ouch");
@@ -16,13 +27,15 @@ public class Delivery : MonoBehaviour
         if (col.CompareTag("Vaccine") && !_hasVaccine)
         {
             _hasVaccine = true;
+            Destroy(col.gameObject, destructionDelay);
             Debug.Log("Vaccine picked up!");
+            _spriteRenderer.color = hasPackageColor;
         }
         else if (col.CompareTag("Patient") && _hasVaccine)
         {
             _hasVaccine = false;
             Debug.Log("Vaccine delivered to patient");
+            _spriteRenderer.color = noPackageColor;
         }
-        
     }
 }
